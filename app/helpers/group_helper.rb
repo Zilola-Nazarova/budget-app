25adddc11cd0fa2bb5e266fbf691ce25440c1cca
zilola <zilolanazarova1994@gmail.com>
2023-11-20 18:47:33 +0500

    Add helper method that validates the icon url and sets default icon if the url is invalid

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -2,4 +2,13 @@ module GroupHelper
   def total_amount(group)
     group.purchases.sum(:amount)
   end
+
+  def check_url(url)
+    default = 'missing_avatar.png'
+    if ((File.extname(url) =~/^(.png|.gif|.jpg)$/ )||(url =~ /^#{URI::regexp}$/))
+      url
+    else
+      default
+    end
+  end
 end
