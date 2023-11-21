f4ba48b04c89a985006759d82545b71365f5f8d0
zilola <zilolanazarova1994@gmail.com>
2023-11-21 16:30:47 +0500

    Update create action to perform redirectios and show error/success flash messages

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,32 +9,27 @@ class PurchasesController < ApplicationController
   end
 
   def new
-    @groups = current_user.groups
-    puts "HERE #{@groups}"
     @purchase = Purchase.new
-    @purchase.group_purchases.build
-    # index = 0
-    # @groups.count.times do
-    #   @purchase.group_purchases.new
-    #   index += 1
-    #   name = @group.name
-    # end
-    # @purchase.group_purchases.build
-    # @group_purchase = GroupPurchase.new
   end
 
   def create
-    @purchase = current_user.purchases.new(purchase_params)
-    # puts "HERE #{purchase_params[:group_purchases_attributes]['0'][:group_id].first}"
-    # puts "HERE #{purchase_params[:group_id]}"
-    @purchase.save!
-
-    # purchase_params[:group_purchases_attributes]['0'][:group_id].each do |id|
-    #   @purchase.group_purchases.new
-    #   group_purchase = GroupPurchase.new(group_id: id, purchase_id: @purchase.id)
-    #   group_purchase.save!
-    # end
-    redirect_to group_purchases_path(params[:group_id])
+    @purchase = current_user.purchases.new(purchase_params.except(:group_ids))
+    @groups = Group.where(id: purchase_params[:group_ids])
+    if @groups.empty?
+      flash.now[:error] = "You must choose at least one category!"
+      render :new
+    else
+      if @purchase.save
+        @groups.each do |group|
+          group.purchases << @purchase
+        end
+        flash[:success] = "Transaction was created and added to #{@groups.length} goups!"
+        redirect_to group_purchases_path(params[:group_id])
+      else
+        flash.now[:error] = @purchase.errors.full_messages.to_sentence
+        render :new
+      end
+    end
   end
 
   def edit; end
@@ -44,11 +39,11 @@ class PurchasesController < ApplicationController
   def destroy
     @purchase = Purchase.find(params[:id])
     @purchase.destroy!
-    flash[:success] = 'Transaction was deleted successfully!'
+    flash[:error] = 'Transaction was deleted successfully!'
     redirect_to group_purchases_url(params[:group_id])
   end
 
   def purchase_params
-    params.require(:purchase).permit(:name, :amount, group_purchases_attributes: :group_id)
+    params.require(:purchase).permit(:name, :amount, group_ids: [])
   end
 end
